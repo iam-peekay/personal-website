3f76d6074d2a156b7b5086feb9d1b28283f77225
Luke Tongue <luke@luketongue.com>
2019-03-10 18:00:52 +0000

    Initial Commit
    Cloned Gatsby starter site from https://github.com/alxshelepenok/gatsby-starter-lumen.
    Updated dev dependencies as blog post sorting was not working when deploying to Netlify.
    Updated various config settings e.g. sitename, description.
    Added image of Preethi.

---
+++ config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  title: 'Blog by Preethi Kasireddy',
+  subtitle: 'Founder & CEO of TruStory. I have a passion for understanding things at a fundamental level and sharing it as clearly as possible.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 6,
+  googleAnalyticsId: '',
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Preethi Kasireddy',
+    photo: '/photo.jpg',
+    bio: 'Founder & CEO of TruStory. I have a passion for understanding things at a fundamental level and sharing it as clearly as possible.',
+    contacts: {
+      email: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '#',
+      vkontakte: '#'
+    }
+  }
+};
