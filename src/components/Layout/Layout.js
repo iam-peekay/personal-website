f0bfb3f58ecfb62b241ae595f3ef2bb899aac79a
Luke Tongue <luke@luketongue.com>
2019-04-10 17:38:04 +0100

    Social Share Meta Data
    Added Twitter card meta tags & Facebook OG meta tags.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import { withPrefix } from 'gatsby';
 import styles from './Layout.module.scss';
 
 const Layout = ({ children, title, description }) => (
@@ -8,6 +9,14 @@ const Layout = ({ children, title, description }) => (
       <html lang="en" />
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content="Preethi Kasireddy" />
+      <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@iam_preethi" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
       <script src="https://cdn.convertkit.com/assets/CKJS4.js?v=21"></script>
     </Helmet>
     {children}
